19c7c876fc26b6f9e167742be81e9cf71b42f8d8
Corentin FANIC <cocofafa85@users.noreply.github.com>
2026-02-13 02:18:35 +0100

    deploy: setup vite base and github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/PortfolioV2/', // Indispensable pour GitHub Pages
     resolve: {
         alias: {
             '@': '/src',
